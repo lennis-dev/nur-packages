ee7e1eb7bd3e6f56e36369c685923849a304538d
Lennis <x@lennis.dev>
2025-04-15 21:36:43 +0200

    Add dev-lennis-www package and remove example-package

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  dev-lennis-www = pkgs.callPackage ./pkgs/dev-lennis-www { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
