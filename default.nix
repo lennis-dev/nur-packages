d1999d27c77c258c46ff6bcba9f04f421d9285b8
Lennis <x@lennis.dev>
2025-09-30 14:41:05 +0200

    Add update-my-nur package with update script

--- default.nix
+++ default.nix
@@ -19,6 +19,7 @@
   dev-lennis-www = pkgs.callPackage ./pkgs/dev-lennis-www { };
   email-obfuscater = pkgs.callPackage ./pkgs/email-obfuscater { };
   hackit = pkgs.callPackage ./pkgs/hackit { };
+  update-my-nur = pkgs.callPackage ./pkgs/update-my-nur { };
   gio-project-avatar-fetcher = pkgs.callPackage ./pkgs/gio-project-avatar-fetcher { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
